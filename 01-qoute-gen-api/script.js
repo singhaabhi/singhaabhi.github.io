c0dccc16d3799035714c52e5b6ebaa9a91c8d7f5
Abhinav Singh <97288513+singhaabhi@users.noreply.github.com>
2024-03-03 17:24:40 +0530

    Add files via upload

--- 01-qoute-gen-api/script.js
+++ 01-qoute-gen-api/script.js
@@ -1,30 +1,53 @@
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const btn = document.getElementById("btn");
+const loader = document.getElementById("loader");
 
-async function getQuote() {
+// Loading function
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Complete function
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
+
+// const apiUrl = "https://api.quotable.io/random";
+
+const newQuote = (apiQuote) => {
+  if (apiQuote.content.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = `"${apiQuote.content}"`;
+  authorText.textContent = `-${apiQuote.author}`;
+};
+
+const getQuote = async () => {
+  loading();
   const apiUrl = "https://api.quotable.io/random";
   try {
     const response = await fetch(apiUrl);
     apiQuote = await response.json();
-    console.log(apiQuote.content);
-    console.log(apiQuote.author);
-
-    quoteText.textContent = `"${apiQuote.content}"`;
-    if (!apiQuote.author) {
-      authorText.textContent = "Unknown";
-    } else {
-      authorText.textContent = `-${apiQuote.author}`;
-    }
-
-    if (apiQuote.content.length > 120) {
-      quoteText.classList.add("long-quote");
-    } else {
-      quoteText.classList.remove("long-quote");
-    }
+    newQuote(apiQuote);
+    console.log(apiQuote);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
-}
+  complete();
+};
+
+getQuote();
 
 btn.addEventListener("click", getQuote);
+
+document.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    getQuote();
+  }
+});
